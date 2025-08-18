6935d111a39c3e9e30678fa9555e57ca5f1e3017
Jurmarcus Allen <me@jurmarcus.com>
2025-08-19 03:51:58 +0900

    agenix 2

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -2,11 +2,14 @@
 {
   imports = [
     inputs.home-manager.darwinModules.home-manager
-    inputs.agenix.homeManagerModules.default
   ];
 
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
+
+  home-manager.sharedModules = [
+    inputs.agenix.homeManagerModules.default
+  ];
 
   home-manager.users.${config.my.primaryUser} = import ./${config.my.primaryUser};
 }

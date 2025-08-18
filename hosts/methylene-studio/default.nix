db1cf304833f46d7c932c2918d2475e9305b2fde
Jurmarcus Allen <allenj@meta.com>
2025-08-18 15:10:23 +0900

    fixes 2

--- hosts/methylene-studio/default.nix
+++ hosts/methylene-studio/default.nix
@@ -6,7 +6,7 @@
 
   my.primaryUser = "methylene";
 
-  users.users.${primaryUser} = {
+  users.users.${config.my.primaryUser} = {
     name = "${config.my.primaryUser}";
     home = "/Users/${config.my.primaryUser}";
     shell = pkgs.fish;

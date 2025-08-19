87e2a2bfb91d05bc1f005f86bce0a2507a82ef09
Jurmarcus Allen <me@jurmarcus.com>
2025-08-19 09:44:25 +0900

    keybinds another

--- modules/darwin/system-keybinds.nix
+++ modules/darwin/system-keybinds.nix
@@ -17,9 +17,17 @@
           };
           "64" = {
             enabled = false;
+            value = {
+              type = "standard";
+              parameters = [ 32 49 1048576 ];
+            };
           };
           "65" = {
             enabled = false;
+            value = {
+              type = "standard";
+              parameters = [ 32 49 1572864 ];
+            };
           };
         };
       };
